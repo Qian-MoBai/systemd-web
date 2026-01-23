684d940c9c8145ad9dd1e8cb1600e79368edc62a
Qian-MoBai <Qian-MoBai@outlook.com>
2026-01-23 19:43:28 +0800

    feat(backend): 初始化 Spring Boot 项目

---
+++ backend/src/main/java/com/mobai/systemd/web/SystemdWebApplication.java
@@ -0,0 +1,18 @@
+package com.mobai.systemd.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Qian-MoBai
+ */
+@SpringBootApplication
+public class SystemdWebApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SystemdWebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mobai/systemd/web/SystemdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobai.systemd.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemdWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
